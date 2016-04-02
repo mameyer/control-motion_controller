4a72b2f1e4396ee0e61ca92beb33d5e7796be1b9
Manuel Meyer <mameyer@uni-bremen.de>
2016-04-02 19:38:33 +0200

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,18 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+namespace motion_controller
+{
+    class DummyClass
+    {
+        public: 
+            /**
+             * Print a welcome to stdout
+             * \return nothing
+             */
+            void welcome();
+    };
+
+} // end namespace motion_controller
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <motion_controller/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    motion_controller::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
